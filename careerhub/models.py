05a8c192e4e2e1d97ec7e5377f92e29780b869f2
Ganesh Maduranathan <ganesh.masa92@gmail.com>
2016-08-09 21:18:33 +0530

    Database to pg and Table migrations

---
+++ careerhub/models.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Market(models.Model):
+    class Meta:
+        db_table = '"markets"'
+    name          = models.CharField(max_length=255,null=False)
+    code          = models.CharField(max_length=255,null=False)
+    currency_code = models.CharField(max_length=255,null=False)
+
+class Locale(models.Model):
+    class Meta:
+        db_table = '"locales"'
+    name     = models.CharField(max_length=255,null=False)
+    code     = models.CharField(max_length=255,null=False)
+    mit_code = models.CharField(max_length=255,null=False)
+    
+class SourceType(models.Model):
+    class Meta:
+        db_table = '"source_types"'
+    name       = models.CharField(max_length=255,null=False)
+    reid_min   = models.IntegerField(null=False)
+    reid_max   = models.IntegerField(null=False)
+    group_name = models.CharField(max_length=255,null=False)
+    
+class Survey(models.Model):
+    class Meta:
+        db_table = '"surveys"'
+    name       = models.CharField(max_length=255,null=False)
+    url        = models.CharField(max_length=255,null=False)
+    market_id  = models.ForeignKey(Market)
+    locale_id  = models.ForeignKey(Locale)
+    is_global  = models.BooleanField(default=False)
+    is_special = models.BooleanField(default=False)
+    begin_at   = models.CharField(max_length=200,null=True)
+    end_at     = models.CharField(max_length=200,null=True)
+    
+class Distribution(models.Model):
+    class Meta:
+        db_table = '"distributions"'
+    name           = models.CharField(max_length=255,null=False)
+    survey_id      = models.ForeignKey(Survey)
+    source_type_id = models.ForeignKey(SourceType)
+    reid           = models.IntegerField(null=False)
+    is_default     = models.BooleanField(default=False)
+    skip_opt_in    = models.BooleanField(default=False)
+    skip_reid      = models.BooleanField(default=False)
--- chadmin/settings.py
+++ chadmin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'careerhub'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'chadmin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'careerhub_dev_python',
+        'USER': 'madg',
+        'PASSWORD': 'gmaddy',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
